28b54dbe32fc7c7512e29c9116cb4532f87ca442
Milkfries <67666583+Milkfries@users.noreply.github.com>
2026-04-30 15:26:24 -0600

    Fix ChessMove to work with autograder

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -11,26 +11,18 @@ public class ChessMove {
     private ChessPosition start;
     private ChessPosition end;
     private ChessPiece.PieceType promotionType;
-    private ChessMove otherMove;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
         this.start = startPosition;
         this.end = endPosition;
         this.promotionType = promotionPiece;
-        this.otherMove = null;
     }
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition) {
         this.start = startPosition;
         this.end = endPosition;
         this.promotionType = null;
-        this.otherMove = null;
     }
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessMove otherMove) {
-        this.start = startPosition;
-        this.end = endPosition;
-        this.promotionType = null;
-        this.otherMove = otherMove;
-    }
+
 
     /**
      * @return ChessPosition of starting location
@@ -46,9 +38,6 @@ public class ChessMove {
         return end;
     }
     
-    public ChessMove getOtherMove(){
-        return otherMove;
-    }
     /**
      * Gets the type of piece to promote a pawn to if pawn promotion is part of this
      * chess move
