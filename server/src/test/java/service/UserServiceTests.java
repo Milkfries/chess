75c7a6200750f3e2b34b5f480601bae66f603bcb
Milkfries <67666583+Milkfries@users.noreply.github.com>
2026-05-04 22:35:18 -0600

    Added Tests for Login

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -2,8 +2,6 @@ package service;
 
 import org.junit.jupiter.api.*;
 
-import com.google.protobuf.ServiceException;
-
 import dataaccess.*;
 import request.*;
 import result.*;
@@ -15,9 +13,15 @@ public class UserServiceTests {
 
     private static RegisterRequest registerRequest1;
     private static RegisterRequest registerRequest2;
-    private static RegisterRequest forgotUsername;
-    private static RegisterRequest forgotPassword;
-    private static RegisterRequest forgotEmail;
+    private static RegisterRequest registerForgotUsername;
+    private static RegisterRequest registerForgotPassword;
+    private static RegisterRequest registerForgotEmail;
+
+    private static LoginRequest loginRequest1;
+    private static LoginRequest loginRequest2;
+    private static LoginRequest loginForgotUsername;
+    private static LoginRequest loginForgotPassword;
+
 
     @BeforeAll
     public static void init(){
@@ -27,21 +31,27 @@ public class UserServiceTests {
 
         registerRequest1 = new RegisterRequest("Michael", "kingofpop123", "jackson1@gmail.com");
         registerRequest2 = new RegisterRequest("Elvis", "rockandroller", "therealpres@gmail.com");
-        forgotUsername = new RegisterRequest(null, "bestbeatle", "fabfour@gmail.com");
-        forgotPassword = new RegisterRequest("bobdylan", null, "completeunknown@gmail.com");
-        forgotEmail = new RegisterRequest("EltonJohn","ogpianoman",null);
+        registerForgotUsername = new RegisterRequest(null, "bestbeatle", "fabfour@gmail.com");
+        registerForgotPassword = new RegisterRequest("bobdylan", null, "completeunknown@gmail.com");
+        registerForgotEmail = new RegisterRequest("EltonJohn","ogpianoman",null);
+
+        loginRequest1 = new LoginRequest("Michael","kingofpop123");
+        loginRequest2 = new LoginRequest("Elvis","rockandroller");
+        loginForgotUsername = new LoginRequest(null, "bestbeatle");
+        loginForgotPassword = new LoginRequest("bobdylan", null);
         
+
     }
 
     @BeforeEach
-    public void setup() throws DataAccessException {
+    public void setup() throws Exception {
         userDAO.clear();
         authDAO.clear();
     }
 
     @Test
     @DisplayName("Register User")
-    public void registerUser() throws DataAccessException, BadRequestException, AlreadyTakenException{ 
+    public void registerUser() throws Exception{ 
         RegisterResult registerResult = userService.register(registerRequest1);
         Assertions.assertNotNull(registerResult);
         Assertions.assertEquals(registerRequest1.username(), registerResult.username());
@@ -51,40 +61,40 @@ public class UserServiceTests {
 
     @Test
     @DisplayName("Register Multiple Users")
-    public void registerMultiUser() throws DataAccessException, BadRequestException, AlreadyTakenException{ 
+    public void registerMultiUser() throws Exception{ 
         RegisterResult registerResult1 = userService.register(registerRequest1);
         RegisterResult registerResult2 = userService.register(registerRequest2);
         Assertions.assertNotEquals(registerResult1,registerResult2);
     }
 
     @Test
-    @DisplayName("No Username")
-    public void noUsername() throws DataAccessException, BadRequestException, AlreadyTakenException{
+    @DisplayName("Register No Username")
+    public void noUsername() throws Exception{
         BadRequestException exception = Assertions.assertThrows(BadRequestException.class,()->{
-            userService.register(forgotUsername);
+            userService.register(registerForgotUsername);
             });
         Assertions.assertEquals("Error: bad request", exception.getMessage());
     }
     @Test
-    @DisplayName("No Password")
-    public void noPassword() throws DataAccessException, BadRequestException, AlreadyTakenException{
+    @DisplayName("Register No Password")
+    public void noPassword() throws Exception{
         BadRequestException exception = Assertions.assertThrows(BadRequestException.class,()->{
-            userService.register(forgotPassword);
+            userService.register(registerForgotPassword);
             });
 
         Assertions.assertEquals("Error: bad request", exception.getMessage());
 
     }
     @Test
-    @DisplayName("No Email")
-    public void noEmail() throws DataAccessException, BadRequestException, AlreadyTakenException{
+    @DisplayName("Register No Email")
+    public void noEmail() throws Exception{
         Assertions.assertDoesNotThrow(()->{
-            userService.register(forgotEmail);
+            userService.register(registerForgotEmail);
             });
     }
     @Test
-    @DisplayName("Duplicate Username")
-    public void duplicateUsername() throws DataAccessException, BadRequestException, AlreadyTakenException{
+    @DisplayName("Register Duplicate Username")
+    public void duplicateUsername() throws Exception{
         RegisterRequest duplicateRequest = new RegisterRequest(registerRequest1.username(), "saferPassword", "dontspamme@gmail.com");
 
         userService.register(registerRequest1);
@@ -96,4 +106,106 @@ public class UserServiceTests {
         Assertions.assertEquals("Error: already taken", exception.getMessage());
 
     }
+
+    @Test
+    @DisplayName("Login")
+    public void loginOnce() throws Exception{
+        Assertions.assertDoesNotThrow(()->{
+            userService.register(registerRequest1);
+            userService.login(loginRequest1);   
+        });
+    }
+    @Test
+    @DisplayName("Login Verified")
+    public void loginCheck() throws Exception{
+        RegisterResult registerResult = userService.register(registerRequest1);
+        LoginResult loginResult = userService.login(loginRequest1);  
+        Assertions.assertEquals(authDAO.getAuth(loginResult.authToken()).username(),registerRequest1.username());
+        Assertions.assertNotEquals(registerResult.authToken(), loginResult.authToken());
+    }
+
+    @Test
+    @DisplayName("Login Multiple")
+    public void loginMultiple() throws Exception{
+        Assertions.assertDoesNotThrow(()->{
+            userService.register(registerRequest1);
+            userService.register(registerRequest2);
+            userService.login(loginRequest1);   
+            userService.login(loginRequest2);
+        });
+    }
+
+    @Test
+    @DisplayName("Login Repeat")
+    public void loginRepeat() throws Exception{
+        userService.register(registerRequest1);
+        LoginResult loginResult1 = userService.login(loginRequest1);   
+        LoginResult loginResult2 = userService.login(loginRequest1);
+
+        Assertions.assertNotEquals(loginResult1.authToken(), loginResult2.authToken());
+        Assertions.assertEquals(loginResult1.username(),loginResult2.username());
+    }
+
+    @Test
+    @DisplayName("Login Multiple Verify")
+    public void loginMultipleCheck() throws Exception{
+        userService.register(registerRequest1);
+        userService.register(registerRequest2);
+        LoginResult loginResult1 = userService.login(loginRequest1);   
+        LoginResult loginResult2 = userService.login(loginRequest2);
+        Assertions.assertNotEquals(loginResult1.authToken(), loginResult2.authToken());
+        Assertions.assertEquals(loginResult1.username(), registerRequest1.username());
+        Assertions.assertEquals(loginResult2.username(), registerRequest2.username());
+    }
+
+    @Test
+    @DisplayName("Login before Register")
+    public void loginNoRegister() throws Exception{
+        Assertions.assertThrows(UnauthorizedException.class, ()->{
+            userService.login(loginRequest1);
+        });
+    }
+
+    @Test
+    @DisplayName("Login wrong Password")
+    public void loginWrongPassword() throws Exception{
+        userService.register(registerRequest1);
+        LoginRequest loginWrongPassword = new LoginRequest(loginRequest1.username(), "iforgot");
+        Assertions.assertThrows(UnauthorizedException.class, ()->{
+            userService.login(loginWrongPassword);
+        });
+    }
+
+    @Test
+    @DisplayName("Login no username")
+    public void loginNoUsername() throws Exception{
+        Assertions.assertThrows(BadRequestException.class, ()->{
+            userService.login(loginForgotUsername);
+        });
+    }
+
+    @Test
+    @DisplayName("Login no Password")
+    public void loginNoPassword() throws Exception{
+        Assertions.assertThrows(BadRequestException.class, ()->{
+            userService.login(loginForgotPassword);
+        });
+    }
+
+    @Test
+    @DisplayName("Login Case Sensative")
+    public void loginCaseSensative() throws Exception{
+        userService.register(registerRequest2);
+        LoginRequest login1 = new LoginRequest(loginRequest2.username().toLowerCase(),loginRequest2.password());
+        LoginRequest login2 = new LoginRequest(loginRequest2.username().toUpperCase(),loginRequest2.password());
+        Assertions.assertThrows(UnauthorizedException.class, ()->{
+            userService.login(login1);
+        });
+        Assertions.assertThrows(UnauthorizedException.class, ()->{
+            userService.login(login2);
+        });
+    }
+
+    
 }
+
