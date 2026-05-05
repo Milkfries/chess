855a82d75c0bf56e832fc1a37a264ed602efb0de
Milkfries <67666583+Milkfries@users.noreply.github.com>
2026-05-05 17:50:03 -0600

    Finished basic setup for list create and join games in the service

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,11 +1,14 @@
 package service;
 
+import com.mysql.cj.exceptions.UnableToConnectException;
+
 import dataaccess.AuthDAO;
 import dataaccess.GameDAO;
 import request.CreateGameRequest;
 import request.JoinGameRequest;
 import request.ListGameRequest;
 import result.CreateGameResult;
+import result.ListGameResult;
 
 public class GameService {
     private final AuthDAO authDAO;
@@ -16,15 +19,15 @@ public class GameService {
         this.gameDAO=gameDAO;
     }
 
-    public ListGameRequest listGames(ListGameRequest request){
+    public ListGameResult listGames(ListGameRequest request) throws UnauthorizedException{
 
         return null;
     }
-    public CreateGameResult createGame(CreateGameRequest request){
+    public CreateGameResult createGame(CreateGameRequest request) throws UnauthorizedException, BadRequestException{
 
         return null;
     }
-    public void joinGame(JoinGameRequest request){
+    public void joinGame(JoinGameRequest request) throws UnauthorizedException, BadRequestException, AlreadyTakenException{
 
     }
     
