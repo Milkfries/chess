64d0ec1ec6948a2f6429e88207f2e634f9db3309
Milkfries <67666583+Milkfries@users.noreply.github.com>
2026-05-05 18:17:22 -0600

    Implemented methods for create, join, and list games

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,14 +1,10 @@
 package service;
 
-import com.mysql.cj.exceptions.UnableToConnectException;
-
-import dataaccess.AuthDAO;
-import dataaccess.GameDAO;
-import request.CreateGameRequest;
-import request.JoinGameRequest;
-import request.ListGameRequest;
-import result.CreateGameResult;
-import result.ListGameResult;
+import chess.ChessGame;
+import dataaccess.*;
+import model.*;
+import request.*;
+import result.*;
 
 public class GameService {
     private final AuthDAO authDAO;
@@ -19,16 +15,76 @@ public class GameService {
         this.gameDAO=gameDAO;
     }
 
-    public ListGameResult listGames(ListGameRequest request) throws UnauthorizedException{
+    public ListGameResult listGames(ListGameRequest request) throws UnauthorizedException, DataAccessException{
+        String authToken = request.authToken();
 
-        return null;
+        AuthData authData = authDAO.getAuth(authToken);
+
+        if(authData == null){
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+
+        ListGameResult listGameResult = new ListGameResult(gameDAO.getAllGames());
+
+        return listGameResult;
     }
-    public CreateGameResult createGame(CreateGameRequest request) throws UnauthorizedException, BadRequestException{
+    public CreateGameResult createGame(CreateGameRequest request) throws UnauthorizedException, BadRequestException, DataAccessException{
+        String authToken = request.authToken();
+        String gameName = request.gameName();
 
-        return null;
+
+        if(gameName == null || gameName.isBlank()){
+            throw new BadRequestException("Error: bad request");
+        }
+
+        AuthData authData = authDAO.getAuth(authToken);
+        
+        if(authData == null){
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+
+        ChessGame newGame = new ChessGame();
+        GameData gameData = new GameData(0,null,null,gameName,newGame);
+        int gameID = gameDAO.insertGame(gameData);
+
+        CreateGameResult createGameResult = new CreateGameResult(gameID);
+        return createGameResult;
     }
-    public void joinGame(JoinGameRequest request) throws UnauthorizedException, BadRequestException, AlreadyTakenException{
+    public void joinGame(JoinGameRequest request) throws UnauthorizedException, BadRequestException, AlreadyTakenException, DataAccessException{
+        String authToken = request.authToken();
+        String teamColor = request.teamColor();
+        int gameID = request.gameID();
 
+
+        AuthData authData = authDAO.getAuth(authToken);
+
+        if(authData == null){
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+
+        if(teamColor == null || teamColor.isBlank() || gameID == 0 ){
+            throw new BadRequestException("Error: bad request");
+        }
+
+        GameData currentGame = gameDAO.getGame(gameID);
+        String whiteUsername = currentGame.whiteUsername();
+        String blackUsername = currentGame.blackUsername();
+        String gameName = currentGame.gameName();
+        ChessGame chessGame = currentGame.game();
+
+        if(teamColor.toUpperCase() == "WHITE"){
+            whiteUsername = authData.username();
+        }
+        else if(teamColor.toUpperCase() == "BLACK"){
+            blackUsername = authData.username();
+        }
+        else{
+            throw new BadRequestException("Error: bad request");
+        }
+
+        GameData newGame = new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame);
+
+        gameDAO.updateGame(newGame);
     }
     
 
