b2019c23251fdabcdf30f796df004ff4365e2040
Milkfries <67666583+Milkfries@users.noreply.github.com>
2026-05-07 22:23:16 -0600

    Implemented and fixed all methods

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,9 +1,13 @@
 package dataaccess;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import com.google.gson.Gson;
 
+import chess.ChessGame;
 import model.GameData;
 
 public class SQLGameDAO implements GameDAO{
@@ -19,36 +23,87 @@ public class SQLGameDAO implements GameDAO{
             String gameJSON = new Gson().toJson(gameData.game());
             var statement = "INSERT INTO gameData (whiteUsername,blackUsername,gameName,game) VALUES (?,?,?,?)";
 
-            try(var preparedStatement = conn.prepareStatement(statement)){
+            try(var preparedStatement = conn.prepareStatement(statement,Statement.RETURN_GENERATED_KEYS)){
                 preparedStatement.setString(1,whiteUsername);
                 preparedStatement.setString(2,blackUsername);
                 preparedStatement.setString(3,gameName);
                 preparedStatement.setString(4,gameJSON);
-                return preparedStatement.executeUpdate();
+                preparedStatement.executeUpdate();
+                ResultSet rs = preparedStatement.getGeneratedKeys();
+                if(rs.next()){
+                    int test = rs.getInt(1);
+                    return test;    
+                }
+                throw new DataAccessException("error: failed to insert gameData");
             }
 
         }
         catch (Exception e){
-            throw new DataAccessException("Failed to insert authData");
+            throw new DataAccessException("error: failed to insert gameData");
         }
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getGame'");
+        try(var conn = DatabaseManager.getConnection()){
+            var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM gameData WHERE gameID = ?";
+            try(var preparedStatement = conn.prepareStatement(statement)){
+                preparedStatement.setInt(1,gameID);
+                ResultSet rs = preparedStatement.executeQuery();
+                if(rs.next()){
+                    return readGameData(rs);
+                }
+            }
+            
+        }
+        catch(Exception e){
+            throw new DataAccessException("error: failed to get gameData");
+        }
+
+        return null;
     }
 
     @Override
     public void updateGame(GameData gameData) throws DataAccessException {
-        deleteGame(gameData.gameID());
-        insertGame(gameData);
+        try(var conn = DatabaseManager.getConnection()){
+            String whiteUsername = gameData.whiteUsername();
+            String blackUsername = gameData.blackUsername();
+            String gameJSON = new Gson().toJson(gameData.game());
+            int gameID = gameData.gameID();
+            var statement = "UPDATE gameData SET whiteUsername = ?, blackUsername = ?, game = ? WHERE gameID = ?";
+
+            try(var preparedStatement = conn.prepareStatement(statement)){
+                preparedStatement.setString(1,whiteUsername);
+                preparedStatement.setString(2,blackUsername);
+                preparedStatement.setString(3,gameJSON);
+                preparedStatement.setInt(4,gameID);
+                preparedStatement.executeUpdate();
+            }
+
+        }
+        catch (Exception e){
+            throw new DataAccessException("error: failed to update gameData");
+        }
     }
 
     @Override
     public Collection<GameData> getAllGames() throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllGames'");
+        Collection<GameData> games = new ArrayList<>();
+        try(var conn = DatabaseManager.getConnection()){
+            var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM gameData";
+            try(var preparedStatement = conn.prepareStatement(statement)){
+                ResultSet rs = preparedStatement.executeQuery();
+                while(rs.next()){
+                    games.add(readGameData(rs));
+                }
+            }
+            
+        }
+        catch(Exception e){
+            throw new DataAccessException("error: failed to get games");
+        }
+
+        return games;
     }
 
     @Override
@@ -63,7 +118,7 @@ public class SQLGameDAO implements GameDAO{
 
         }
         catch (Exception e){
-            throw new DataAccessException("Failed to delete gameData");
+            throw new DataAccessException("error: failed to delete gameData");
         }
     }
 
@@ -78,8 +133,16 @@ public class SQLGameDAO implements GameDAO{
 
         }
         catch (Exception e){
-            throw new DataAccessException("Failed to delete gameData");
+            throw new DataAccessException("error: failed to clear gameData");
         }
     }
-    
+    private GameData readGameData(ResultSet rs) throws Exception{
+        int gameID = rs.getInt("gameID");
+        String whiteUsername = rs.getString("whiteUsername");
+        String blackUsername = rs.getString("blackUsername");
+        String gameName = rs.getString("gameName");
+        var gameJSON = rs.getString("game");
+        ChessGame game = new Gson().fromJson(gameJSON, ChessGame.class);
+        return new GameData(gameID,whiteUsername,blackUsername,gameName,game);
+    }
 }
