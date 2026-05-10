3b3ec75f8f5825257909d84923845aa81d266d8e
Milkfries <67666583+Milkfries@users.noreply.github.com>
2026-05-10 16:47:52 -0600

    Implemented logout method

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,23 @@
 package client;
 
 import org.junit.jupiter.api.*;
+
+import request.*;
+import result.*;
 import server.Server;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("localhost", port);
     }
 
     @AfterAll
@@ -21,9 +26,11 @@ public class ServerFacadeTests {
     }
 
 
-    @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
-    }
+    // @Test
+    // public void sampleTest() {
+    //     LoginRequest request = new LoginRequest();
+    //     String result = facade.login(request);
+    //     Assertions.assertTrue(true);
+    // }
 
 }
