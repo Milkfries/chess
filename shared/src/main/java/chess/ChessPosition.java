dfc675c02cad4ffdef88f9eabe1a981e5570aa8d
Milkfries <67666583+Milkfries@users.noreply.github.com>
2026-05-17 09:11:58 -0600

    Renamed variables to be camelCase

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -36,9 +36,9 @@ public class ChessPosition {
         return new ChessPosition(row, col);
     }
     public ChessPosition add(int[] direction,int scale){
-        int new_row =  (this.row + scale * direction[0]);
-        int new_col =  (this.col + scale * direction[1]);
-        return new ChessPosition(new_row, new_col);
+        int newRow =  (this.row + scale * direction[0]);
+        int newCol =  (this.col + scale * direction[1]);
+        return new ChessPosition(newRow, newCol);
     }
 
     public boolean inBoard(){
