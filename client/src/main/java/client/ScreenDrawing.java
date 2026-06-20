6d91f844f88f68614fda3afbedb498f778e0db0d
Milkfries <67666583+Milkfries@users.noreply.github.com>
2026-06-19 22:42:51 -0600

    Added edges with row/column data. Added flag for board to flip to black perspective

--- client/src/main/java/client/ScreenDrawing.java
+++ client/src/main/java/client/ScreenDrawing.java
@@ -9,11 +9,17 @@ import chess.ChessGame.TeamColor;
 import model.GameData;
 
 public class ScreenDrawing {
+    private final static String[] BOARD_LETTERS = {" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "};
     private static PrintStream out;
 
     // Pass in print stream from client
     public static void initStream(PrintStream ps){
         out = ps;
+    }
+    public static void clearScreen(){
+        setBlack();
+        out.print(ERASE_SCREEN);
+        setType();
     }
     //Chessboard Drawing
     public static void drawGame(GameData game, ChessGame.TeamColor color){
@@ -27,19 +33,25 @@ public class ScreenDrawing {
         drawChessBoard(game.game(),color == TeamColor.WHITE ? false : true);
         setType();
     }
-    private static void drawChessBoard(ChessGame game, boolean flipBoard){
-        for(int row = 8; row > 0; row--){
-            for(int space = 0; space <3; space++){
-                for(int col = 1; col < 9; col++){
-                    boolean blankRow = true;
-                    if(space == 1){
-                        blankRow = false;
-                    }
+    private static void drawChessBoard(ChessGame game, boolean flipBoard){// Printing
+        int rowStart = flipBoard ? 1 : 8;
+        int rowEnd = flipBoard ? 8 : 1;
+        int rowStep = flipBoard ? 1 : -1;
+
+        printEdge(flipBoard);
+        for(int row = rowStart; flipBoard ? (row <= rowEnd) : (row >= rowEnd) ; row+=rowStep){ // Flips the direction it reads board if from black's perspective
+            for(int space = 0; space < 3; space++){
+                boolean blankRow = space != 1 ? true : false;
+                printEdgeBox(row, row, blankRow, flipBoard);
+                for(int col = 8; col > 0; col--){
                     printPiece(game, row, col,blankRow);
                 }
+                printEdgeBox(row, 0, blankRow, flipBoard);
                 out.print("\n");
             }  
         }
+        printEdge(flipBoard);
+
         setType();
     }
     private static void printPiece(ChessGame game, int row, int col, boolean blankRow){
@@ -55,19 +67,21 @@ public class ScreenDrawing {
         }
         out.print(EMPTY);
         if(blankRow){
-            out.print(EMPTY);
+            out.print(EMPTY_BIG);
         }
         else{
             out.print(getPieceCode(piece));
+            setRegular();
         }
         out.print(EMPTY);
         setBlack();
     }
     private static String getPieceCode(ChessPiece piece){
         if(piece == null){
-            return EMPTY;
+            return EMPTY_BIG;
         }
         if(piece.getTeamColor() == ChessGame.TeamColor.WHITE){
+            setBold();
             switch (piece.getPieceType()) {
                 case ChessPiece.PieceType.PAWN:
                     return WHITE_PAWN;
@@ -86,6 +100,7 @@ public class ScreenDrawing {
             }
         }
         else{
+            setBold();
             switch (piece.getPieceType()) {
                 case ChessPiece.PieceType.PAWN:
                     return BLACK_PAWN;
@@ -103,36 +118,71 @@ public class ScreenDrawing {
                     return null;
             }
         }
+    }
+    private static void printEdge(boolean flipBoard){
+        int rowStart = flipBoard ? 8 : -1;
+        int rowEnd = flipBoard ? -1 : 8;
+        int rowStep = flipBoard ? -1 : 1;
 
-        
+        for(int j = 0; j < 3; j++){
+            for(int i = rowStart; flipBoard ? (i >= rowEnd) : (i <= rowEnd); i+=rowStep){
+                boolean blankRow = j != 1 ? true : false; 
+                printEdgeBox(0,i,blankRow,flipBoard);
+            }
+            out.print("\n");
+        }
+    }
+    private static void printEdgeBox(int row, int col, boolean blankRow, boolean flipBoard){
+        setGreyType();
+        out.print(EMPTY);
+        if(!blankRow){
+            setBold();
+            if(row == 0){
+                try{
+                    out.print(BOARD_LETTERS[col]);
+                }
+                catch (Exception e){
+                    out.print(EMPTY_BIG);
+                }
+                
+            }
+            else{
+                out.print(" " + row + " ");
+            }
+            setRegular();
+        }
+        else{
+            out.print(EMPTY_BIG);
+        }
+        out.print(EMPTY);
+        setBlack();
     }
 
     // Basic drawing functions
-    public static void setType(){
+    private static void setType(){
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
     }
-    public static void setBlueType(){
+    private static void setBlueType(){
         out.print(SET_BG_COLOR_BLUE);
         out.print(SET_TEXT_COLOR_BLACK);
     }
-    public static void setWhiteType(){
+    private static void setGreyType(){
+        out.print(SET_BG_COLOR_DARK_GREY);
+        out.print(SET_TEXT_COLOR_LIGHT_GREY);
+    }
+    private static void setWhiteType(){
         out.print(SET_BG_COLOR_WHITE);
         out.print(SET_TEXT_COLOR_BLACK);
     }
-
-    public static void setBlack(){
+    private static void setBold(){
+        out.print(SET_TEXT_BOLD);
+    }
+    private static void setRegular(){
+        out.print(RESET_TEXT_BOLD_FAINT);
+    }
+    private static void setBlack(){
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_BLACK);
-    }
-
-    public static void setBlink(){
-        out.print(SET_TEXT_BLINKING);
-    }
-
-    public static void clearScreen(){
-        setBlack();
-        out.print(ERASE_SCREEN);
-        setType();
     }
 }
