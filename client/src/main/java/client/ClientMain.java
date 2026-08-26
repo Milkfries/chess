a141de87769364c3da6da7896d9a895e20a4f924
Milkfries <thebossdylanhale@gmail.com>
2026-08-26 11:09:05 -0600

    Allow easy change of hostname

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,7 +2,9 @@ package client;
 
 public class ClientMain {
     public static void main(String[] args){
-        Client client = new Client(args);
+        String hostName = args.length >= 2 ? args[1] : "localhost";
+        String port = args.length >= 1 ? args[0] : "8080";
+        Client client = new Client(hostName, port);
         client.run();
     }
 }
