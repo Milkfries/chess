a4b01e9478931e939e823e2858a9a51f4aaf0d69
Milkfries <thebossdylanhale@gmail.com>
2026-08-29 11:26:11 -0600

    Add string for pawn promotion

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -3,27 +3,30 @@ package websocket.commands;
 public class MakeMoveCommand extends UserGameCommand{
     private String startPosition;
     private String endPosition;
+    private String promotionPiece;
 
-    public MakeMoveCommand(String authToken, Integer gameID, String startPosition, String endPosition) {
+    public MakeMoveCommand(String authToken, Integer gameID, String startPosition, String endPosition, String promotionPiece) {
         super(CommandType.MAKE_MOVE, authToken,gameID);
         this.startPosition = startPosition;
         this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
     }
-
     public String getStartPosition(){
         return startPosition;
     }
     public String getEndPosition(){
         return endPosition;
     }
-
-
+    public String getPromotionPiece() {
+        return promotionPiece;
+    }
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = super.hashCode();
         result = prime * result + ((startPosition == null) ? 0 : startPosition.hashCode());
         result = prime * result + ((endPosition == null) ? 0 : endPosition.hashCode());
+        result = prime * result + ((promotionPiece == null) ? 0 : promotionPiece.hashCode());
         return result;
     }
 
@@ -48,9 +51,13 @@ public class MakeMoveCommand extends UserGameCommand{
                 return false;
         } else if (!endPosition.equals(other.endPosition))
             return false;
+        if (promotionPiece == null) {
+            if (other.promotionPiece != null)
+                return false;
+        } else if (!promotionPiece.equals(other.promotionPiece))
+            return false;
         return true;
     }
-
     
 
 }
