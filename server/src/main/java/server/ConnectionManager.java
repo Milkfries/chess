37673af5e211de74052dba4ef3175b66ab0bf2ec
Milkfries <thebossdylanhale@gmail.com>
2026-08-29 11:28:32 -0600

    Add data structure for holding multiple connections per game

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -4,27 +4,37 @@ import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.eclipse.jetty.websocket.api.Session;
+import com.google.gson.Gson;
 
 import websocket.messages.ServerMessage;
-
 public class ConnectionManager {
-    public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
-
-    public void add(Session session) {
-        connections.put(session, session);
+    public final ConcurrentHashMap<Integer, ConcurrentHashMap<String,Session>> connections = new ConcurrentHashMap<>(); // game id - > username (maybe authToken) - > Session
+    public Gson serializer = new Gson();
+    public void add(int gameID, String authToken, Session session) {
+        ConcurrentHashMap<String,Session> gameConnections = connections.get(gameID);
+        if(gameConnections == null){
+            gameConnections = new ConcurrentHashMap<>();
+        }
+        
+        gameConnections.put(authToken,session);
+        connections.put(gameID, gameConnections);
     }
 
-    public void remove(Session session) {
-        connections.remove(session);
+    public void remove(int gameID, String authToken) {
+        connections.get(gameID).remove(authToken);
     }
 
-    public void broadcast(Session excludeSession, ServerMessage serverMsg) throws IOException {
-        String msg = serverMsg.toString();
-        for (Session c : connections.values()) {
+    public void broadcast(int gameID, Session currentSession, ServerMessage serverMsg, boolean includeCurrent) throws IOException {
+        String msg = serializer.toJson(serverMsg);
+        for (Session c : connections.get(gameID).values()) {
             if (c.isOpen()) {
-                if (!c.equals(excludeSession)) {
+                if (!c.equals(currentSession)) {
                     c.getRemote().sendString(msg);
                 }
+                else if(includeCurrent){
+                    c.getRemote().sendString(msg);
+                }
+
             }
         }
     }
